19b5ed8db10fae854b4804d0b07eaed890b6b27f
Satoshi Fujinaga <b.fujinaga@gmail.com>
2017-04-25 22:39:55 +0900

    fix sample py scrip SparkContext初期化

--- sandbox/my_script.py
+++ sandbox/my_script.py
@@ -1,3 +1,9 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("My App")
+sc = SparkContext(conf = conf)
+
 lines = sc.textFile("/opt/spark/README.md")
 pythonLines = lines.filter(lambda line: "Python" in line)
-pythonLines.first()
+print(pythonLines.first())
+print(pythonLines.count())
